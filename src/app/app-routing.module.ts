bd0f580a35dfb0da26c83571fe0215468b2377cc
joaHeller <joa.heller@bbzbl-it.ch>
2024-05-30 16:13:57 +0200

    pages anordnen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { DatalistComponent } from './cook/cook.component';
 import { Page1Component } from './pages/page1/page1.component';
 import { Page3Component } from './pages/page3/page3.component';
-import { Page4Component } from './pages/page4/page4.component'; 
+import { Page4Component } from './pages/page4/page4.component';
 import { Page2Component } from './pages/page2/page2.component';
 import { appCanActivate } from './guard/app.auth.guard';
 import { AppRoles } from './app.roles';
 import { Page5Component } from './pages/page5/page5.component';
 import { Page6Component } from './pages/page6/page6.component';
+import { Page7Component } from './pages/page7/page7.component';
+import { Page8Component } from './pages/page8/page8.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DatalistComponent
-
+    component: DatalistComponent,
   },
 
   {
@@ -22,7 +23,7 @@ const routes: Routes = [
     component: Page1Component,
     pathMatch: 'full',
     canActivate: [appCanActivate],
-    data: { roles: [AppRoles.Admin] }
+    data: { roles: [AppRoles.Admin] },
   },
 
   {
@@ -37,21 +38,35 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
-  path: 'page4',
-  component: Page4Component,
-  pathMatch: 'full'
-},
+    path: 'page4',
+    component: Page4Component,
+    pathMatch: 'full',
+  },
 
-{
-  path: 'page5',
-  component: Page5Component,
-  pathMatch: 'full'
-},
-{
-  path: 'page6',
-  component: Page6Component,
-  pathMatch: 'full'
-}
+  {
+    path: 'page5',
+    component: Page5Component,
+    pathMatch: 'full',
+  },
+  {
+    path: 'page6',
+    component: Page6Component,
+    pathMatch: 'full',
+  },
+  {
+    path: 'page7',
+    component: Page7Component,
+    pathMatch: 'full',
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Admin] },
+  },
+  {
+    path: 'page8',
+    component: Page8Component,
+    pathMatch: 'full',
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Admin] },
+  },
 ];
 
 @NgModule({
@@ -59,5 +74,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
-
-
