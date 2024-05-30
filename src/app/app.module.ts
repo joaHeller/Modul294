b7bb92159b058373f26c659f4143626146ba9609
joaHeller <joa.heller@bbzbl-it.ch>
2024-05-30 14:57:34 +0200

    page 7 und 8 compilieren

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { IsInRoleDirective } from './dir/is.in.role.dir';
 import { IsInRolesDirective } from './dir/is.in.roles.dir';
 import { RouterModule } from '@angular/router';
 import { Page6Component } from './pages/page6/page6.component';
+import { Page7Component } from './pages/page7/page7.component';
+import { Page8Component } from './pages/page8/page8.component';
 
 
 export const authConfig: AuthConfig = {
@@ -45,7 +47,7 @@ export function storageFactory(): OAuthStorage {
 
 
 @NgModule({
-  declarations: [AppComponent, DatalistComponent, Page1Component, Page2Component, Page3Component, IsInRoleDirective, IsInRolesDirective, Page6Component],
+  declarations: [AppComponent, DatalistComponent, Page1Component, Page2Component, Page3Component, IsInRoleDirective, IsInRolesDirective, Page6Component, Page7Component, Page8Component],
   imports: [BrowserModule, RouterModule, AppRoutingModule, MatButtonModule, HttpClientModule, MatTableModule, MatFormFieldModule, MatInputModule,FormsModule, ReactiveFormsModule,OAuthModule.forRoot({resourceServer: {sendAccessToken: true}}),],
   providers: [provideAnimationsAsync(), {provide: AuthConfig, useValue: authConfig},
     {provide: HTTP_INTERCEPTORS, useClass: HttpXSRFInterceptor, multi: true},
